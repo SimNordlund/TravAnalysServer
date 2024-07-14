0a05f4bdf45c7e9ea64420e8e125dc61e6549f52
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2024-07-14 21:24:21 +0200

    Started created needed class for rest api + server for RadarHorse.

--- src/main/java/com/example/travanalysserver/controller/RadarHorseController.java
+++ src/main/java/com/example/travanalysserver/controller/RadarHorseController.java
@@ -1,4 +1,24 @@
 package com.example.travanalysserver.controller;
 
+import com.example.travanalysserver.dto.radarhorse.RadarHorseDTO;
+import com.example.travanalysserver.entity.RadarHorse;
+import com.example.travanalysserver.service.interfaces.RadarHorseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/radar") //Byt namn sen? XD
 public class RadarHorseController {
+
+    private final RadarHorseService radarHorseService;
+
+    @GetMapping("/find/{id}")
+    public RadarHorseDTO getRadarHorseById(@PathVariable Long id){
+        return radarHorseService.getRadarHorseById(id);
+    }
+
+ //   @PostMapping("/store/{id}")
+ //   public RadarHorse saveRadarHorseById(@)
+
 }
