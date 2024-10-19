5fe2bbf066bd8403421c7fd1636684dc4aeea8f7
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2024-10-19 15:52:58 +0200

    added restapi for testing

--- src/main/java/com/example/travanalysserver/controller/HorseController.java
+++ src/main/java/com/example/travanalysserver/controller/HorseController.java
@@ -1,13 +1,14 @@
 package com.example.travanalysserver.controller;
 
 import com.example.travanalysserver.dto.horse.HorseDTO;
+import com.example.travanalysserver.entity.testing.Horse;
+import com.example.travanalysserver.repository.HorseRepo;
 import com.example.travanalysserver.service.interfaces.HorseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -16,9 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class HorseController {
 
     private final HorseService horseService;
+    private final HorseRepo horseRepo;
 
     @GetMapping("/{id}")
     public HorseDTO getHorseById(@PathVariable Long id) {
         return horseService.getHorseById(id);
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<String> addHorse(@RequestBody Horse horse) {
+        horseRepo.save(horse);
+        return new ResponseEntity<>("Horse added successfully", HttpStatus.CREATED);
+    }
+
+    @PostMapping("/send2/{name}")
+    public ResponseEntity<String> addHorse(@PathVariable String name) {
+        System.out.println("Metoden fungerar");
+        horseRepo.save(new Horse(null, name));  // Save horse with only a name
+        return new ResponseEntity<>("Horse added successfully", HttpStatus.CREATED);
     }
 }
