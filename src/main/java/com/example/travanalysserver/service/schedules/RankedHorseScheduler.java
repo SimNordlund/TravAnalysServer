86695c2fba8ebe452c99d3292749c6b226079641
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-05-08 21:35:36 +0200

    Viva la vida.

--- src/main/java/com/example/travanalysserver/service/schedules/RankedHorseScheduler.java
+++ src/main/java/com/example/travanalysserver/service/schedules/RankedHorseScheduler.java
@@ -18,7 +18,7 @@ public class RankedHorseScheduler {
         this.controller = controller;
     }
 
-    @Scheduled(cron = "0 0 0/1 * * *")
+    @Scheduled(cron = "0 0/10 * * * *")
     public void runEveryFiveMinutes() {
         logger.info("Hämtar uppdaterd data ifrån GameChanger");
         controller.saveAllRanked();
