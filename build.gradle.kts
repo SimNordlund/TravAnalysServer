e9769e009be5741ee64fd0aa42e729f11d2a6026
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-05-15 12:54:10 +0200

    Dependency clean up

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,23 +31,19 @@ dependencies {
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.mysql:mysql-connector-j")
-	runtimeOnly("com.h2database:h2")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation ("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-validation:3.0.5")
 	implementation("com.fasterxml.jackson.core:jackson-core:2.17.1")
 	implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
 	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.1")
 	implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.17.1")
-	implementation("com.rabbitmq:amqp-client:5.21.0")
 	implementation("com.fasterxml.jackson.core:jackson-annotations:2.17.1")
 
 
 	implementation("org.yaml:snakeyaml")
 	implementation("com.github.javafaker:javafaker:1.0.2") { exclude ("org.yaml") }
-	implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect")
 	implementation("org.modelmapper:modelmapper:3.2.0")
 
 	implementation("org.springframework.boot:spring-boot-starter-mail")
@@ -62,7 +58,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	testImplementation("org.springframework.security:spring-security-test:6.3.0")
-	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
 }
 
 tasks.withType<Test> {
