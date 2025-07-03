424aab88e1782029e8baca70a4d391420677fd1c
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-07-03 21:31:18 +0200

    Started creating emailcontroller

---
+++ src/main/java/com/example/travanalysserver/controller/EmailController.java
@@ -0,0 +1,26 @@
+package com.example.travanalysserver.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/emailToBeStored")
+public class EmailController {
+
+    // private final EmailService emailService
+    // private final EmailRepo emailRepo
+
+ /*   @PostMapping("/storeEmail")
+    public ResponseEntity<String> storeEmail(@RequestBody UserEmail userEmail) {
+        //spara repo
+        //returnera responsentity, se completehorsecontroller
+    } */
+
+
+    //SKAPA DTO OCKSÅ!!
+}
