c7a59361dfb0eb6f99efb5ae3084980be293c4f6
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-08-12 23:44:56 +0200

    eat my dog

--- src/main/java/com/example/travanalysserver/entitysec/RankHorseView.java
+++ src/main/java/com/example/travanalysserver/entitysec/RankHorseView.java
@@ -4,34 +4,19 @@ import java.time.LocalDateTime;
 
 public interface RankHorseView {
         Long getId();
-
         Integer getDateRankedHorse();
-
         String getTrackRankedHorse();
-
         String getLapRankedHorse();
-
         Integer getNr();
-
         String getNameRankedHorse();
-
         String getAnalysRankedHorse();
-
         String getTidRankedHorse();
-
         String getPrestationRankedHorse();
-
         String getMotstandRankedHorse();
-
         String getPrispengarRankedHorse();
-
         LocalDateTime getUpdatedAt();
-
         String getTipsRankedHorse();
-
         String getStallSkrikRankedHorse();
-
         String getPlaceringRankedHorse();
-
         String getFormRankedHorse();
 }
