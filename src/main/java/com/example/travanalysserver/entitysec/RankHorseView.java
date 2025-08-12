c0d6faa5e5369bff84aa7f99a89d2bd1e1eda0a4
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-08-12 23:41:56 +0200

    is this the answer then ai wont take our jobs

--- src/main/java/com/example/travanalysserver/entitysec/RankHorseView.java
+++ src/main/java/com/example/travanalysserver/entitysec/RankHorseView.java
@@ -4,19 +4,34 @@ import java.time.LocalDateTime;
 
 public interface RankHorseView {
         Long getId();
+
         Integer getDateRankedHorse();
+
         String getTrackRankedHorse();
+
         String getLapRankedHorse();
+
         Integer getNr();
+
         String getNameRankedHorse();
+
         String getAnalysRankedHorse();
+
         String getTidRankedHorse();
+
         String getPrestationRankedHorse();
+
         String getMotstandRankedHorse();
+
         String getPrispengarRankedHorse();
+
         LocalDateTime getUpdatedAt();
+
         String getTipsRankedHorse();
+
         String getStallSkrikRankedHorse();
-        String getPlaceringRankedHorse(); //ändrat efter
-        String getFormRankedHorse(); //änrat efter rh allltså RankHorse
+
+        String getPlaceringRankedHorse();
+
+        String getFormRankedHorse();
 }
