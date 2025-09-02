463cb9d348a188a0bc98b455e903d07f6487f75f
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-09-02 14:38:22 +0200

    ORN update + no dublicates of speltips.

--- src/main/java/com/example/travanalysserver/repository/CompleteHorseRepo.java
+++ src/main/java/com/example/travanalysserver/repository/CompleteHorseRepo.java
@@ -5,10 +5,12 @@ import com.example.travanalysserver.entity.CompleteHorse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface CompleteHorseRepo extends JpaRepository<CompleteHorse, Long> {
 
     List<CompleteHorse> findAllByLap_Id(Long lapId);
@@ -28,7 +30,27 @@ public interface CompleteHorseRepo extends JpaRepository<CompleteHorse, Long> {
         join h.starts s
       where t.date = :date
         and s.tips >= :minTips
+        and s.id = (  
+            select max(s2.id)             
+            from Starts s2
+            where s2.completeHorse.id = h.id
+              and s2.tips = (              
+                  select max(s3.tips)
+                  from Starts s3
+                  where s3.completeHorse.id = h.id
+              )
+              and s2.analys = (            
+                  select max(s4.analys)
+                  from Starts s4
+                  where s4.completeHorse.id = h.id
+                    and s4.tips = (
+                        select max(s5.tips) from Starts s5 where s5.completeHorse.id = h.id
+                    )
+              )
+        )
       order by t.nameOfTrack, c.nameOfCompetition, l.nameOfLap, h.numberOfCompleteHorse
       """)
-    List<SkrallarHorseDto> findByDateAndMinTips(@Param("date") LocalDate date, @Param("minTips") int minTips);
+    List<SkrallarHorseDto> findByDateAndMinTips(@Param("date") LocalDate date,
+                                                @Param("minTips") int minTips);
 }
+
