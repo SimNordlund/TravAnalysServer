39a6e47d10fce5247c97a785af0a9b5dc708e078
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2026-03-22 09:28:45 +0100

    Added columns to AppUrl table

--- src/main/java/com/example/travanalysserver/entitysec/AppUrl.java
+++ src/main/java/com/example/travanalysserver/entitysec/AppUrl.java
@@ -6,8 +6,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
 
-
-
 @Entity
 @Table(name = "AppUrl", schema = "public")
 @NoArgsConstructor
@@ -21,6 +19,22 @@ public class AppUrl {
     private Long id;
     @Column(name = "url")
     private String url;
+    @Column(name = "avd1") //Varje avd1 här avser en sträng av de top 3 bästa hästarna i varje lopp i bestämt spel
+    private String avd1;
+    @Column(name = "avd2")
+    private String avd2;
+    @Column(name = "avd3")
+    private String avd3;
+    @Column(name = "avd4")
+    private String avd4;
+    @Column(name = "avd5")
+    private String avd5;
+    @Column(name = "avd6")
+    private String avd6;
+    @Column(name = "avd7")
+    private String avd7;
+    @Column(name = "avd8")
+    private String avd8;
     @Column(name = "date")
     private String date;
 }
